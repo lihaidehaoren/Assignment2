d144662e16702499a3bb7b189bda4128f1b2184d
lihaidehaoren <529509927@qq.com>
2022-10-24 17:14:49 +0800

    1

--- LargeNumberAddition.java
+++ LargeNumberAddition.java
@@ -7,21 +7,72 @@ public class LargeNumberAddition {
         int n;
         n=input.nextInt();
 
+        while (n>0) {
+            String D = sc.nextLine();
+            String[] arr3 = D.split(" ");
+            int[] d = new int[arr3.length];
+            for (int i = 0; i < d.length; i++) {
+                d[i] = Integer.parseInt(arr3[i]);
+            }
             int p,q;
-            p=input.nextInt();
-            q=input.nextInt();
+            p=d[0];
+            q=d[1];
 
+            String A = sc.nextLine();     //输入数组A的数值 长度为p
+            String[] arr1 = A.split(" ");
+            int[] a = new int[arr1.length];
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Integer.parseInt(arr1[i]);
+            }
 
-            String A =sc.nextLine();     //输入数组A的数值 长度为p
-            String [] arr1 = A.split(" ");
-            int [] a =new int[arr1.length];
-            for(int i=0;i<a.length;i++){
-                a[i]=Integer.parseInt(arr1[i]);
-                System.out.print(a[i]);
+            String B = sc.nextLine();     //输入数组B的数值 长度为q
+            String[] arr2 = B.split(" ");
+            int[] b = new int[arr2.length];
+            for (int i = 0; i < b.length; i++) {
+                b[i] = Integer.parseInt(arr2[i]);
+            }
 
+            if (p >=q){
+                int[]c=new int[p];
+                for (int i=0;i<p;i++){
+                    c[i]=a[i];
+                }
+                for (int i=0;i<q;i++){
+                    c[p-i-1]=c[p-i-1]+b[q-i-1];
+                }
+                for (int i=1;i<p;i++){
+                    if (c[i]>=10){
+                        c[i]=c[i]-10;
+                        c[i-1]=c[i-1]+1;
+                    }
+                }
+                for(int i=0;i<c.length;i++){
+                    System.out.print(c[i]);
+                }
+                System.out.println();
+            }
+            else {
+                int[]c=new int[q];
+                for (int i=0;i<q;i++){
+                    c[i]=b[i];
+                }
+                for (int i=0;i<p;i++){
+                    c[q-i-1]=c[q-i-1]+a[p-i-1];
+                }
+                for (int i=1;i<q;i++){
+                    if (c[i]>=10){
+                        c[i]=c[i]-10;
+                        c[i-1]=c[i-1]+1;
+                    }
+                }
+                for(int i=0;i<c.length;i++){
+                    System.out.print(c[i]);
+                }
+                System.out.println();
+            }
 
-
+            n=n-1;
         }
-
     }
 }
+
