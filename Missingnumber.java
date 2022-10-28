331d84ec169b080ac50a325e52e727646fe8bc00
lihaidehaoren <529509927@qq.com>
2022-10-28 19:48:39 +0800

    Update Missingnumber.java

--- Missingnumber.java
+++ Missingnumber.java
@@ -5,15 +5,25 @@ public class Missingnumber {
         Scanner sc =new Scanner(System.in);
         Scanner input =new Scanner(System.in);
 
+
+
+
+        String B = sc.nextLine();     //输入n的数值
+        String[] arr2 = B.split(" ");
+        int[] b = new int[arr2.length];
+        for (int i = 0; i < b.length; i++) {
+            b[i] = Integer.parseInt(arr2[i]);
+        }
+
         boolean flag =false;
         int n;
-        n= input.nextInt();
+        n= b[0];
         boolean D[] =new boolean[n];
         for (int i=0;i<n;i++){
             D[i] =false;                      //初始化为false
         }
 
-        String A = sc.nextLine();     //输入数组A的数值 长度为p
+        String A = sc.nextLine();             //输入数组
         String[] arr1 = A.split(" ");
         int[] a = new int[arr1.length];
         for (int i = 0; i < a.length; i++) {
